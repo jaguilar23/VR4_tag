b12875dd670570911b8828ed5d0deed41749b0d6
Kathryn King <kate1012k@gmail.com>
2024-02-28 21:42:20 -0600

    eermmmmm nothin

--- VR4_Proj1/Assets/Scripts/PowerUps/Teleport.cs
+++ VR4_Proj1/Assets/Scripts/PowerUps/Teleport.cs
@@ -6,10 +6,24 @@ public class Teleport : MonoBehaviour
 {
     public float teleportDistance = 3f; // Distance to teleport
     private Transform playerTransform;
+    public int powerUpCredits = 0;
 
     void Start()
     {
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform; // Assuming player has a "Player" tag
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U) && powerUpCredits > 0)
+        {
+            // move the player
+           /*
+            Vector3 teleportPosition = playerTransform.position + playerTransform.forward * teleportDistance;
+            playerTransform.position = teleportPosition;
+           */
+            powerUpCredits--;
+        }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -17,8 +31,11 @@ public class Teleport : MonoBehaviour
         if (collision.gameObject.CompareTag("Player")) // If collided object is the player
         {
             Debug.Log("collide");
+            powerUpCredits++;
+           
             Vector3 teleportPosition = playerTransform.position + playerTransform.forward * teleportDistance;
             playerTransform.position = teleportPosition;
+           
         }
     }
 }
